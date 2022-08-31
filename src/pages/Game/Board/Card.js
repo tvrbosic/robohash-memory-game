e0815bc17718fe429bcbaa049942064d1a020771
tvrbosic <tomislav.vrbosic@gmail.com>
2022-08-31 10:16:51 +0200

    Generate grid hashes and display card images

--- src/pages/Game/Board/Card.js
+++ src/pages/Game/Board/Card.js
@@ -1,7 +1,13 @@
 import styles from './Card.module.css';
 
-const Card = () => {
-  return <div className={styles.Card}></div>;
+const Card = (props) => {
+  return (
+    <div className={styles.Card}>
+      <div className={styles.ImageBackground}>
+        <img src={`https://robohash.org/${props.hash}.png?size=200x200`} className={styles.Image} />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/Game/Board/Grid.js
+++ src/pages/Game/Board/Grid.js
@@ -1,10 +1,13 @@
 import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
 import styles from './Grid.module.css';
 import Card from './Card';
+import shuffleArray from '../../../utility/shuffle-array';
 
 const Grid = () => {
   const boardSize = parseInt(useSelector((state) => state.game.cardsCount));
+
   let boardSizeStyle;
   switch (boardSize) {
     case 16:
@@ -20,8 +23,22 @@ const Grid = () => {
       boardSizeStyle = styles['Grid-4x4'];
   }
 
+  const cardHashesArray = () => {
+    const hashes = [];
+    // Generate array of boardSize/2 unique hashes
+    for (let index = 0; index < boardSize / 2; index++) {
+      hashes.push(uuidv4());
+    }
+    // Create duplicates of those hashes in same array
+    hashes.push(...hashes);
+    // Shuffle array elements
+    shuffleArray(hashes);
+    return hashes;
+  };
+  
+
   const generateCards = () => {
-    return [...Array(boardSize)].map((elementInArray, index) => <Card key={index}>Test</Card>);
+    return cardHashesArray().map((hash, index) => <Card key={index} hash={hash} />);
   };
 
   return <div className={`${styles.Container} ${boardSizeStyle}`}>{generateCards()}</div>;
