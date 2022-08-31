eb1a1075005c702ad1098a933682a83be900615f
tvrbosic <tomislav.vrbosic@gmail.com>
2022-08-31 17:48:20 +0200

    Flip card effect on click

--- src/pages/Game/Board/Card.js
+++ src/pages/Game/Board/Card.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import styles from './Card.module.css';
 
 const Card = (props) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const cardClickHandler = () => {
+    setIsActive((previousIsActive) => {
+      return !previousIsActive;
+    });
+  };
+
+  const cardContentStyles = isActive ? `${styles.Content} ${styles.Active}` : `${styles.Content}`;
+
   return (
-    <div className={styles.Card}>
-      <div className={styles.ImageBackground}>
-        <img src={`https://robohash.org/${props.hash}.png?size=200x200`} className={styles.Image} />
+    <div className={styles.Card} onClick={cardClickHandler}>
+      <div className={cardContentStyles}>
+        <div className={styles.Front}>
+        </div>
+        <div className={styles.Back}>
+          <img
+            src={`https://robohash.org/${props.hash}.png?size=200x200`}
+            className={styles.Image}
+          />
+        </div>
       </div>
     </div>
   );
