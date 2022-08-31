beb45f6ed344aa887d1b53e22be5a9f12887317e
tvrbosic <tomislav.vrbosic@gmail.com>
2022-08-31 08:16:16 +0200

    Generate grid based on selected board size

--- src/pages/Game/Board/Grid.js
+++ src/pages/Game/Board/Grid.js
@@ -4,12 +4,27 @@ import styles from './Grid.module.css';
 import Card from './Card';
 
 const Grid = () => {
-  const boardSize = useSelector((state) => state.game.cardsCount);
+  const boardSize = parseInt(useSelector((state) => state.game.cardsCount));
+  let boardSizeStyle;
+  switch (boardSize) {
+    case 16:
+      boardSizeStyle = styles['Grid-4x4'];
+      break;
+    case 24:
+      boardSizeStyle = styles['Grid-6x4'];
+      break;
+    case 36:
+      boardSizeStyle = styles['Grid-6x6'];
+      break;
+    default:
+      boardSizeStyle = styles['Grid-4x4'];
+  }
+
   const generateCards = () => {
-    return [...Array(parseInt(boardSize))].map((elementInArray, index) => <Card key={index}>Test</Card>);
+    return [...Array(boardSize)].map((elementInArray, index) => <Card key={index}>Test</Card>);
   };
 
-  return <div className={styles.Container}>{generateCards()}</div>;
+  return <div className={`${styles.Container} ${boardSizeStyle}`}>{generateCards()}</div>;
 };
 
 export default Grid;
