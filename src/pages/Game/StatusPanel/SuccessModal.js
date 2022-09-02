a306fa2ff97c5d60baddce51bac4c15b7419bff1
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-02 12:52:14 +0200

    Success modal basic styling

--- src/pages/Game/StatusPanel/SuccessModal.js
+++ src/pages/Game/StatusPanel/SuccessModal.js
@@ -4,9 +4,12 @@ import Button from '../../../components/Button';
 const BackModal = (props) => {
   return (
     <div className={styles.Container}>
-      <div className={styles.Text}>
-        <p>Congratulations!</p>
-        <p>You have found all matches and achieved score (less is better): {props.playerScore}</p>{' '}
+      <div className={styles.Message}>
+        <h2 className={styles.Heading}>Congratulations!</h2>
+        <p>You have found all matches.</p>
+        <p>
+          Achieved score: <span className={styles.Score}>{props.playerScore}</span>
+        </p>
       </div>
       <div className={styles.Controls}>
         <Button text='Continue' onClick={props.onConfirm} />
