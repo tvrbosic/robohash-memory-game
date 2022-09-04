685ca8e056026c1c77bc12de7ea445d105855f3a
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-04 09:55:59 +0200

    useHttpRequest custom hook

---
+++ src/hooks/use-http.js
@@ -0,0 +1,39 @@
+import { useState, useCallback } from 'react';
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, handleData) => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error('Request resulted with an error!');
+      }
+
+      const data = await response.json();
+
+      // Success callback (do something with returned data)
+      handleData(data);
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
