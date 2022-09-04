c3f0fe22354a758cecdf5412e529bb86da138627
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-04 17:58:59 +0200

    Highscores loading spinner, README.md update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # RoboHash Memory Game
 Memory game which generates cards using [RoboHash Web Service](https://robohash.org/).
 
+Application was created with [React](https://reactjs.org/), backend is simulated with [Json Server](https://github.com/typicode/json-server)
+
 ## Usage
 1. Clone repository: `git clone git@github.com:tvrbosic/robohash-memory-game.git`
 2. Install npm packages: `cd robohash-memory-game && npm install`
@@ -13,3 +15,4 @@ Memory game which generates cards using [RoboHash Web Service](https://robohash.
 - [React Router](https://reactrouter.com/en/main)
 - [Json Server](https://github.com/typicode/json-server)
 - [UUID](https://www.npmjs.com/package/uuid)
+- [React Spinners](https://www.npmjs.com/package/react-spinners)
--- src/pages/Highscores/Highscores.js
+++ src/pages/Highscores/Highscores.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FadeLoader } from 'react-spinners';
 
 import styles from './Highscores.module.css';
 import Button from '../../components/Button';
 import useHttpRequest from '../../hooks/use-http-request';
 
+const spinnerStyle = {
+  margin: '10rem auto',
+}
+
 const Highscores = () => {
   const [playerHighscores, setPlayerHighscores] = useState([]);
-  const { sendRequest } = useHttpRequest(); // Extract and handle isLoading & error
+  const { isLoading, sendRequest } = useHttpRequest(); // Extract and handle isLoading & error
   let navigate = useNavigate();
-
 
   useEffect(() => {
     sendRequest(
@@ -29,25 +33,28 @@ const Highscores = () => {
       <div className={styles.Content}>
         <h1 className={styles.Heading}>Highscores</h1>
         <p className={styles.Description}>Describe how is player score calculated.</p>
-        <div className={styles.ScoresTable}>
-          <div className={styles.TableHeader}>
-            <div className={styles.LeftColumn}>Position</div>
-            <div className={styles.CenterColumn}>Player</div>
-            <div className={styles.RightColumn}>Score</div>
+        <FadeLoader color={'navy'} loading={isLoading} cssOverride={spinnerStyle} size={150} />
+        {!isLoading && (
+          <div className={styles.ScoresTable}>
+            <div className={styles.TableHeader}>
+              <div className={styles.LeftColumn}>Position</div>
+              <div className={styles.CenterColumn}>Player</div>
+              <div className={styles.RightColumn}>Score</div>
+            </div>
+            {playerHighscores.map((item, index) => {
+              return (
+                <div className={styles.TableItem} key={index}>
+                  <div className={styles.LeftColumn}>{index + 1}</div>
+                  <div className={styles.CenterColumn}>{item.player}</div>
+                  <div className={styles.RightColumn}>{item.score}</div>
+                </div>
+              );
+            })}
+            <div className={styles.ButtonsPanel}>
+              <Button text='Return to menu' onClick={returnClickHandler} />
+            </div>
           </div>
-          {playerHighscores.map((item, index) => {
-            return (
-              <div className={styles.TableItem} key={index}>
-                <div className={styles.LeftColumn}>{index + 1}</div>
-                <div className={styles.CenterColumn}>{item.player}</div>
-                <div className={styles.RightColumn}>{item.score}</div>
-              </div>
-            );
-          })}
-          <div className={styles.ButtonsPanel}>
-            <Button text='Return to menu' onClick={returnClickHandler} />
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
