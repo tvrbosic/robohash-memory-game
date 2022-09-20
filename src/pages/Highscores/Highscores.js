82ed547f02923616e989dc19c480f054619ec7e8
tvrbosic <tomislav.vrbosic@gmail.com>
2022-09-20 11:19:45 +0200

    Scoreboard for different board sizes

--- src/pages/Highscores/Highscores.js
+++ src/pages/Highscores/Highscores.js
@@ -4,8 +4,16 @@ import { FadeLoader } from 'react-spinners';
 
 import styles from './Highscores.module.css';
 import useHttpRequest from '../../hooks/use-http-request';
+import Select from '../../components/Select';
 import Scoreboard from './Scoreboard';
 import FetchError from './FetchError';
+
+const boardSizes = [
+  { id: 0, value: 'A', text: 'All' },
+  { id: 1, value: 'S', text: '16 cards' },
+  { id: 2, value: 'M', text: '24 cards' },
+  { id: 3, value: 'L', text: '36 cards' },
+];
 
 const spinnerStyle = {
   margin: '14rem auto',
@@ -13,31 +21,63 @@ const spinnerStyle = {
 
 const Highscores = () => {
   const [playerHighscores, setPlayerHighscores] = useState([]);
+  const [boardSize, setBoardSize] = useState('A');
   const { isLoading, error, sendRequest } = useHttpRequest(); // Extract and handle isLoading & error
   let navigate = useNavigate();
 
   useEffect(() => {
-    sendRequest(
-      {
-        url: 'http://localhost:3001/highscores?_sort=score&_order=desc&_start=0&_end=10',
-      },
-      setPlayerHighscores
-    );
-  }, [sendRequest]);
+    if (boardSize === 'A') {
+      sendRequest(
+        {
+          url: 'http://localhost:3001/highscores?_sort=score&_order=desc&_start=0&_end=10',
+        },
+        setPlayerHighscores
+      );
+    } else {
+      sendRequest(
+        {
+          url: `http://localhost:3001/highscores?board=${boardSize}&_sort=score&_order=desc&_start=0&_end=10`,
+        },
+        setPlayerHighscores
+      );
+    }
+  }, [boardSize, sendRequest]);
 
   const returnClickHandler = () => {
     navigate('/');
+  };
+
+  const selectBoardSizeHandler = (event) => {
+    setBoardSize(event.target.value);
   };
 
   return (
     <div className={styles.Container}>
       <div className={styles.Content}>
         <h1 className={styles.Heading}>Highscores</h1>
-        <FadeLoader color={'navy'} loading={isLoading} cssOverride={spinnerStyle} size={150} />
+        <div className={styles.SelectBoardContainer}>
+          <label htmlFor="board-size">Board size</label>
+          <Select
+            id="board-size"
+            options={boardSizes}
+            onChange={selectBoardSizeHandler}
+          />
+        </div>
+        <FadeLoader
+          color={'navy'}
+          loading={isLoading}
+          cssOverride={spinnerStyle}
+          size={150}
+        />
         {!isLoading && !error && (
-          <Scoreboard playerHighscores={playerHighscores} onReturnClick={returnClickHandler} />
+          <Scoreboard
+            playerHighscores={playerHighscores}
+            onReturnClick={returnClickHandler}
+          />
         )}
-        {!isLoading && error && <FetchError onReturnClick={returnClickHandler} />}
+        {!isLoading && error && (
+          <FetchError onReturnClick={returnClickHandler} />
+        )}
       </div>
     </div>
   );
